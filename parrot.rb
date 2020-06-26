3937de5021eccf06afe198796f8c01ba3933ac12
Adi Skop <adiskop@gmail.com>
2020-06-26 11:05:44 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(string="Squawk!")
-  puts "#{string}"
+def parrot(noise="Squawk!")
+  puts "The voice I make is#{noise}"
   return parrot
 end
